28c4d5f5fc3b4e87e350eed3eb6fde8530db6e3b
Dominik <101473446+b14ucky@users.noreply.github.com>
2024-06-01 11:39:54 +0200

    feat: add new branch and background textures (#13)

--- Branch.cpp
+++ Branch.cpp
@@ -12,14 +12,17 @@ Branch::Branch(std::string texturePath, int level, int yAxis)
 	this->texture = new sf::Texture;
 	this->texture->loadFromFile(texturePath);
 	this->setTexture(*this->texture);
-	this->side = randomizeSide();
+
+	// make sure that first branch doesn't collide with lumberjack otherwise player would lose instantly
+	this->side = level == 1 ? 0 : randomizeSide(); 
 	
-	if (this->side == 0) { // 0 = left
-		this->xAxis = 50; // CHECK IF LOOKS GOOD
+	if (this->side == 0) { // 0 = right
+		this->xAxis = 215;
 	}
-	else // 1 = right
+	else // 1 = left
 	{
-		this->xAxis = 230; // CHECK IF LOOKS GOOD
+		this->xAxis = 345; // CHECK IF LOOKS GOOD
+		this->setScale(-1, 1);
 	}
 
 	this->setPosition(xAxis, yAxis);
