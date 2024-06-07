e9558b790a29225c9efae11ac18d8365ad64eee5
Dominik <101473446+b14ucky@users.noreply.github.com>
2024-06-07 12:44:48 +0200

    refactor: use enum class for side instead of bool values

--- Lumberjack.h
+++ Lumberjack.h
@@ -2,10 +2,13 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "Side.h"
+
 class Lumberjack : public sf::Sprite
 {
 private:
     sf::Texture *texture;
+    Side side;
     sf::Vector2f positionLeft;
     sf::Vector2f positionRight;
     void initPositions();
