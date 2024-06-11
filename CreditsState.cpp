5c86c49b7f07912499afcfd0a6608a2f81d343b3
Bartek Janoszka <bartlomiej.janoszka@gmail.com>
2024-06-11 20:33:08 +0200

    Added authors names

--- CreditsState.cpp
+++ CreditsState.cpp
@@ -11,6 +11,7 @@ CreditsState::CreditsState(sf::RenderWindow* window, StateManager* stateManager,
     this->initBackground();
     this->initVariables();
     this->initButtons();
+    this->initText();
     
 }
 
@@ -26,14 +27,33 @@ void CreditsState::initVariables()
 }
 void CreditsState::initButtons()
 {
-    this->creditsButton.push_back(Button("BACK", this->font, 40, sf::Color(244, 176, 123), 3, sf::Color(19, 9, 2), sf::Vector2f(this->window->getSize().x / 2, 500)));
+    this->creditsButton.push_back(Button("BACK", this->font, 45, sf::Color(244, 176, 123), 3, sf::Color(19, 9, 2), sf::Vector2f(this->window->getSize().x / 2, 380)));
 }
-void CreditsState::initText() {
-    
+void CreditsState::initText() 
+{
+    this->authorText.setString("AUTHORS:");
+    this->authorsNamesText.setString("- Bartlomiej Janoszka\n- Dominik Meisner\n- Karol Piechaczek");
+    this->authorText.setCharacterSize(55);
+    this->authorsNamesText.setCharacterSize(40);
+    this->authorText.setFont(this->font);
+    this->authorsNamesText.setFont(this->font);
+    this->authorText.setOutlineColor(sf::Color(19, 9, 2));
+    this->authorsNamesText.setOutlineColor(sf::Color(19, 9, 2));
+    this->authorText.setFillColor(sf::Color(244, 176, 123));
+    this->authorsNamesText.setFillColor(sf::Color(244, 176, 123));
+    this->authorText.setOutlineThickness(3);
+    this->authorsNamesText.setOutlineThickness(3);
+    sf::Vector2f textBounds = this->authorText.getGlobalBounds().getSize();
+    this->authorText.setOrigin(textBounds.x / 2, textBounds.y / 2);
+    this->authorText.setPosition(sf::Vector2f(this->window->getSize().x / 2, 35));
+    sf::Vector2f textBounds2 = this->authorsNamesText.getGlobalBounds().getSize();
+    this->authorsNamesText.setOrigin(textBounds2.x / 2, textBounds2.y / 2);
+    this->authorsNamesText.setPosition(sf::Vector2f(this->window->getSize().x / 2, 135));
 }
 void CreditsState::renderCreditsContent()
 {
-    this->window->draw(this->creditsText);
+    this->window->draw(this->authorText);
+    this->window->draw(this->authorsNamesText);
 
     for (auto& button : creditsButton) 
     {
